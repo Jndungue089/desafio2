cb68bff50b77dcd6ed8314e7ed4ce35b61c98d93
Jndungue089 <68353090+Jndungue089@users.noreply.github.com>
2024-09-19 21:03:09 +0100

    Initial Commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { CardComponent } from "../../components/card/card.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavbarComponent, CardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
